f4008c9104c80424b87394efd3d2286efad94033
Asher <32278830+athaun@users.noreply.github.com>
2020-06-08 14:07:51 -0500

    Update runGPU.sh

--- CViralTrans/viralTransv2.0/runGPU.sh
+++ CViralTrans/viralTransv2.0/runGPU.sh
@@ -2,7 +2,7 @@
 clear
 echo "Running test"
 screen -S viralTransGPU bash -c "tmux new-session \; \
-                          send-keys './multithread.sh' C-m \; \
+                          send-keys './multirun.sh' C-m \; \
                           split-window -h \; \
                           send-keys 'htop' C-m \; \
                           split-window -v \;"
