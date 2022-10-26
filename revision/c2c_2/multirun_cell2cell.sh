e30b1d50a48763cbd7226e99ca8f9a4bf1ac2a75
athaun <asher.haun@outlook.com>
2022-10-25 20:58:42 -0500

    c2c_2

---
+++ revision/c2c_2/multirun_cell2cell.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+declare -a parameters=(
+  0.03
+  0.1
+  0.3
+  0.8
+  1.0
+  1.3
+  1.7
+  2.0
+  3.0
+  5.0
+  10.0
+)
+
+for i in "${parameters[@]}"
+do
+  echo "[SHELL] Running test with parameter $i";
+  nvcc Hazard.cu -o cell2cell_${i}.out && ./cell2cell_${i}.out $i;
+  echo "[SHELL] test with parameter $i is done."
+done
